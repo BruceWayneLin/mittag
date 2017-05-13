556fe461a1e6a1af884a92cebde21458d3820eff
linSkanda <anikiwayne@gmail.com>
2017-05-13 11:27:46 +0800

    first committed to upload the web

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mittag/admin/');
+define('HTTP_CATALOG', 'http://localhost/mittag/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mittag/admin/');
+define('HTTPS_CATALOG', 'http://localhost/mittag/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/mittag/admin/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/mittag/image/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/mittag/admin/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/mittag/admin/view/template/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/config/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/storage/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/storage/upload/');
+define('DIR_CATALOG', '/Applications/XAMPP/xamppfiles/htdocs/mittag/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mittag');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mittag/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mittag/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/mittag/catalog/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/mittag/image/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/mittag/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/mittag/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/config/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/storage/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/XAMPP/xamppfiles/htdocs/mittag/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mittag');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
